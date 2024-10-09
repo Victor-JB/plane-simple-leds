db3e1396905c2158d0deaaf7631c25a031f56a82
Victor Joulin-Batejat <victorjb2015@gmail.com>
2024-10-09 12:44:52 -0700

    Created flask app for simple led controller, going to host it on raspi

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # plane simple leds
- 
+
+Querying flight API for flight info
+
+# plane-simple-led-controller flask app
+
+A Flask app
+
+# Getting Started
+
+## `make install`
+
+- install dependencies
+
+## `make start`
+
+- start flask server
+
+## `make lint`
+
+- lint code with black
+
+
+---
+
+Generated using [create-flask-app](https://github.com/drizzleco/create-flask-app)
--- plane_incoming.py
+++ plane_incoming.py
@@ -16,20 +16,22 @@ PLANE_API_KEY = os.getenv('PLANE_API_KEY')
 
 format_string = '%Y-%m-%dT%H:%M:%S'
 
+iata_code = "SJC"
+
 # The API endpoint
-url = f"https://aviation-edge.com/v2/public/timetable?key={PLANE_API_KEY}&iataCode=SJC&type=arrival&status=active"
+url = f"https://aviation-edge.com/v2/public/timetable?key={PLANE_API_KEY}&iataCode={iata_code}&type=arrival&status=active"
 
 # A GET request to the API
 arr_dict_list = requests.get(url).json()
 
-arriving_soon = []
+arrival_queue = []
 for arrival in arr_dict_list:
     arr_time = arrival['arrival']['estimatedTime'].split(".")[0] # raw string arrival time
-    print(arr_time)
     arr_datetime = datetime.strptime(arr_time, format_string)
-    print(arr_datetime)
 
     if datetime.now() < arr_datetime:
-        arriving_soon.append(arrival)
+        arrival_queue.append({"est_time": arr_datetime, "flight": arrival['flight']})
 
-print(arriving_soon)
+arrival_queue.sort(key = lambda x:x['est_time'])
+
+for flight in arrival_queue: print("\n", flight)
